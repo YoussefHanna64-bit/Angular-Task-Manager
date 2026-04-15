2989f172f7536602244302767f18a28f7afa123e
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-04-15 22:31:05 +0200

    Feat: implement task input with data binding

--- src/app/components/input/Input.ts
+++ src/app/components/input/Input.ts
@@ -1,8 +1,49 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Task {
+  title: string;
+  desc: string;
+  priority: 'low' | 'medium' | 'high';
+  date: string;
+  category: 'work' | 'personal' | 'study';
+  tags: string;
+}
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.html',
   styleUrl: './input.css',
+  imports: [FormsModule],
 })
-export class Input {}
+export class Input {
+  title: string = '';
+  desc: string = '';
+  priority: 'low' | 'medium' | 'high' = 'low';
+  date: string = '';
+  category: 'work' | 'personal' | 'study' = 'work';
+  tags: string = '';
+
+  tasksList: Task[] = [];
+
+  submit() {
+    const newTask: Task = {
+      title: this.title,
+      desc: this.desc,
+      priority: this.priority,
+      date: this.date,
+      category: this.category,
+      tags: this.tags,
+    };
+
+    this.tasksList.push(newTask);
+    console.log(this.tasksList);
+
+    this.title = '';
+    this.desc = '';
+    this.priority = 'low';
+    this.date = '';
+    this.category = 'work';
+    this.tags = '';
+  }
+}
