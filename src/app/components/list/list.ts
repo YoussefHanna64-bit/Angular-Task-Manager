4ec54bebc2524703410b030078f8290ed3271b78
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-04-17 17:27:38 +0200

    Feat:  add the tabs and rename card class to taskCard

--- src/app/components/list/list.ts
+++ src/app/components/list/list.ts
@@ -10,4 +10,42 @@ import { Task } from '../../models/taskModel';
 })
 export class List {
   @Input() tasksList: Task[] = [];
+
+  ngOnInit() {
+    const newTask: Task = {
+      title: 'Task 1',
+      desc: 'Description for Task 1',
+      date: new Date('2024-12-31').toISOString().split('T')[0],
+      priority: 'low',
+      category: 'work',
+      tags: '',
+      isDone: false,
+    };
+
+    this.tasksList.push(newTask);
+  }
+
+  currentTab: 'all' | 'not-done' | 'done' = 'all';
+
+  get filteredTasks(): Task[] {
+    if (this.currentTab === 'done') {
+      return this.tasksList.filter((t) => t.isDone);
+    }
+    if (this.currentTab === 'not-done') {
+      return this.tasksList.filter((t) => !t.isDone);
+    }
+    return this.tasksList;
+  }
+
+  setTab(tab: 'all' | 'not-done' | 'done') {
+    this.currentTab = tab;
+  }
+
+  Edit(task: Task) {
+    console.log('Editing task:', task.title);
+  }
+
+  Delete(task: Task) {
+    console.log('Deleting task:', task.title);
+  }
 }
